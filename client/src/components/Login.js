1eed355c7b1e3c721e59399bcb07e6c7e7e422e6
Logan Negley <logannegley@gmail.com>
2020-03-20 18:22:42 -0500

    form div in login and credentials state set with object: username password

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+
+const [credentials, setCredentials] = useState({
+  username:'',
+  password:''
+})
+  // form functions 
+  const handleChanges = event => {
+
+  }
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className='login-form'>
+      <form>
+
+      </form>
+    </div>
   );
 };
 
