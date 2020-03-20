9c1a0e2bbe1b81de9ae69da74ec31614c64e76b3
Logan Negley <logannegley@gmail.com>
2020-03-20 18:17:26 -0500

    PrivateRoute component created and PrivateRoute added to app.js to route to BubblePage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./styles.scss";
 
 import Login from "./components/Login";
-import "./styles.scss";
+import PrivateRoute from '../src/components/PrivateRoute';
+import BubblePage from '../src/components/BubblePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute path='/bubblePage' component={BubblePage}/>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
